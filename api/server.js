925946bbe8d49e9ec0fddc4099019ea083a4538e
abc <abc@example.com>
2024-05-24 03:04:25 +0100

    Hostme

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 
 // middleware to connect with frontend
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://hostme-application.vercel.app/",
     credentials: true,
 }));
 
