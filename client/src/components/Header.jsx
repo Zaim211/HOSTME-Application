f013b20c85ab05ee00e9df0b0d754f4cfd1edcc2
Zaim <Yousseftitazaim@gmail.com>
2024-05-24 21:28:00 +0100

    Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,103 +1,69 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "../UserContext";
 import logo from "../assets/logo.svg";
+import { UserContext } from "../UserContext";
+import { useContext } from "react";
+import NavMobile from "./NavMobile";
+import { useMediaQuery } from 'react-responsive';
 
-export default function NavMobile() {
-  const { user } = useContext(UserContext);
-  const [showHeader, setShowHeader] = useState(false);
+export default function Header() {
+  const {user} = useContext(UserContext);
 
-  // Function to toggle the visibility of the header
-  const toggleHeader = () => {
-    setShowHeader(!showHeader);
-  };
+  const isMobileDevice = useMediaQuery({ query: '(max-width: 768px)' });
 
-  // Function to hide the header when clicking on a menu item
-  const hideHeader = () => {
-    setShowHeader(false);
-  };
+  if (isMobileDevice) {
+    return (
+      <header className="wrapper mt-12 border-b">
+        <NavMobile />
+      </header>
+    );
+  }
 
   return (
-    <nav className={showHeader ? "bg-primary" : "bg-transparent"}>
-      <div className="fixed top-0 right-4 flex flex-col justify-center items-end p-4">
-        {/* Button to toggle the visibility of the header */}
-        <button className="text-xl" onClick={toggleHeader}>
-          {showHeader ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-              />
-            </svg>
-          )}
-        </button>
-        {showHeader && (
-          <nav className="flex flex-col mt-2 p-4 items-end bg-gray-600 rounded-2xl">
-            <Link to={"/account"} className="my-2" onClick={hideHeader}>
-              Profile
-            </Link>
-            <div className="border-b border-gray-300 w-full my-2"></div>
-            <Link to={"/account/hostings"} className="my-2" onClick={hideHeader}>
-              Your Host
-            </Link>
-            <div className="border-b border-gray-300 w-full my-2"></div>
-            <Link to={"/account/places"} className="my-2" onClick={hideHeader}>
-              Add Places
-            </Link>
-            <div className="border-b border-gray-300 w-full my-2"></div>
-            <Link
-              to={user ? "/account" : "/login"}
-              className="flex items-center gap-2 mb-2 border border-gray-300 rounded-full py-2 px-4"
-              onClick={hideHeader}
-            >
-              <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6 relative top-1"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              {!!user && <div className="h2-bold">{user.name}</div>}
-            </Link>
-          </nav>
-        )}
+    <header className="w-full border-b">
+       <div className="wrapper flex items-center justify-between">
+      <Link to={"/"} className="w-36">
+        <img src={logo} alt="logo" width={150} height={28} />
+      </Link>
+      <div className="flex-between-center mt-1">
+        <Link to={user?'/account':'/login'} className="h2-bold">Profile</Link>
+        <div className="border-l border-gray-300"></div>
+        <Link to={user?'/account/hostings':'/login'} className="h2-bold">Listings</Link>
+        <div className="border-l border-gray-300"></div>
+        <Link to={user?'/account/places':'/login'} className="h2-bold">Add Places</Link>
       </div>
-      <div className="fixed top-0 left-0 flex flex-col justify-center items-end p-2">
-        <Link to={"/"} className="mt-2 md:p2">
-          <img src={logo} alt="logo" width={120} height={40} />
-        </Link>
+
+      <Link to={user?'/account':'/login'} className="flex items-center gap-2 mb-2 border border-gray-300 rounded-full py-2 px-4 ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+        <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6 relative top-1"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        {!!user && <div className="h2-bold">{user.name}</div>}
+      </Link>
       </div>
-    </nav>
+    </header>
   );
-}
+};
