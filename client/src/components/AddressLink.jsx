b073df802471fbc0bcce593c005f824d34425b55
Zaim <Yousseftitazaim@gmail.com>
2024-05-26 20:23:17 +0100

    AddressLink component for displaying address links

--- client/src/components/AddressLink.jsx
+++ client/src/components/AddressLink.jsx
@@ -3,6 +3,7 @@ export default function AddressLink({children,className=null}) {
       className = 'my-3 block';
     }
     className += ' flex gap-1 font-semibold underline';
+
     return (
       <p className={className} target="_blank" href={'https://maps.google.com/?q='+children}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
