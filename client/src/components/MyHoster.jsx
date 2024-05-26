d54523bef82f12d96189bc159eb138bab12dc44e
Zaim <Yousseftitazaim@gmail.com>
2024-05-26 20:28:18 +0100

    HostingsPage component to display hosted places

--- client/src/components/MyHoster.jsx
+++ client/src/components/MyHoster.jsx
@@ -1,72 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import axios from "axios";
-// import PlaceGallery from "./PlaceGallery";
-// import HostingDates from "./HostingDates";
-
-// export default function HostingsPage() {
-//   const [hostings, setHostings] = useState([]);
-
-//   useEffect(() => {
-//     axios.get("/hosted").then((response) => {
-//       setHostings(response.data);
-//     });
-//   }, []);
-
-//   return (
-//     <div>
-//       <div className="p-4">
-//         {hostings?.length > 0 &&
-//           hostings.map((hosting) => (
-//             <Link
-//               key={hosting._id}
-//               to={`/account/hostings/${hosting._id}`}
-//               className="flex bg-gray-200 rounded-2xl overflow-hidden mt-2 space-y-8 flex-row-2 p-2"
-//             >
-//               <div className="w-50 mt-4">
-//                 <PlaceGallery place={hosting.place} />
-//               </div>
-//               <div className="grow">
-//                 <h2 className="h2-bold mt-0">{hosting.place.title}</h2>
-//                 <div className="text-xl">
-//                   <HostingDates
-//                     hosting={hosting}
-//                     className="mb-2 mt-4 text-gray-500"
-//                   />
-//                   <div className="flex">
-//                     <div>
-//                       Name:<h2 className="h2-bold">{hosting.name}</h2>
-//                       Phone: <p className="p-20-semibold">{hosting.phone}</p>
-//                     </div>
-//                     <div>
-//                       <svg
-//                         xmlns="http://www.w3.org/2000/svg"
-//                         fill="none"
-//                         viewBox="0 0 24 24"
-//                         strokeWidth={1.5}
-//                         stroke="currentColor"
-//                         className="w-8 h-8"
-//                       >
-//                         <path
-//                           strokeLinecap="round"
-//                           strokeLinejoin="round"
-//                           d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
-//                         />
-//                       </svg>
-//                       <span className="text-2xl">
-//                         Total price: ${hosting.price}
-//                       </span>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </Link>
-//           ))}
-//       </div>
-//     </div>
-//   );
-// }
-
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -113,5 +44,4 @@ export default function HostingsPage() {
       </div>
     </div>
   );
-}
-
+};
