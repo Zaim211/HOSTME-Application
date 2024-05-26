0717a3160a0fc57d3e60778bf7f976ab2f1c12a4
Zaim <Yousseftitazaim@gmail.com>
2024-05-26 20:10:57 +0100

    Update AppController.js
    Test the connection

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 class AppController {
-  // Test the connection
+  // Test the connection route
    static async test(req, res) {
     mongoose.connect(process.env.MONGO_URL);
     return res.json({ message: 'Test ok' });
