b1e951c27cc970251af1761fec5af035fd0dd47b
Zaim <Yousseftitazaim@gmail.com>
2024-05-26 20:30:50 +0100

    Implement Perks component for selecting accommodation perks

--- client/src/components/Perks.jsx
+++ client/src/components/Perks.jsx
@@ -7,6 +7,7 @@ export default function Perks({ selected, onChange }) {
       onChange(selected.filter((item) => item !== name));
     }
   }
+  
   return (
     <>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
