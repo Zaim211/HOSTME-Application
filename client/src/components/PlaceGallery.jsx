437f5d36406ebadfa14490b261277d93ac6707d1
Zaim <Yousseftitazaim@gmail.com>
2024-06-06 23:39:56 +0100

    Update PlaceGallery.jsx

--- client/src/components/PlaceGallery.jsx
+++ client/src/components/PlaceGallery.jsx
@@ -10,7 +10,7 @@ export default function PlaceGallery({ place }) {
       <div className="absolute inset-0 bg-black text-white min-h-screen">
         <div className="bg-black p-8 grid gap-4">
           <div>
-            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
+            <h2 className="text-3xl mr-48">Photos of {place?.title}</h2>
             <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                 <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
@@ -18,7 +18,7 @@ export default function PlaceGallery({ place }) {
               Close photos
             </button>
           </div>
-          {place?.photos?.length > 0 && place.photos.map((photo, index) => (
+          {place?.photos?.length > 0 && place.photos?.map((photo, index) => (
             <div key={photo.id || index}>
               <Image src={photo} key={index} alt=""/>
             </div>
@@ -32,18 +32,18 @@ export default function PlaceGallery({ place }) {
     <div className="relative">
       <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
         <div>
-          {place.photos?.[0] && (
+          {place?.photos?.[0] && (
             <div>
               <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={place.photos[0]} alt=""/>
             </div>
           )}
         </div>
         <div className="grid">
-          {place.photos?.[1] && (
+          {place?.photos?.[1] && (
             <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={place.photos[1]} alt=""/>
           )}
           <div className="overflow-hidden">
-            {place.photos?.[2] && (
+            {place?.photos?.[2] && (
               <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={place.photos[2]} alt=""/>
             )}
           </div>
