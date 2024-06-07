9efe5fc8c7c6916173cdff18039da644389f7e90
Zaim <Yousseftitazaim@gmail.com>
2024-06-07 20:09:55 +0100

    Update HostingsPage.jsx

--- client/src/pages/HostingsPage.jsx
+++ client/src/pages/HostingsPage.jsx
@@ -64,7 +64,7 @@ export default function HostingsPage() {
                       className="text-red-500"
                       onClick={() => deleteHosting(hosting._id)}
                     >
-                      <svg
+                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -78,18 +78,6 @@ export default function HostingsPage() {
                         d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                       />
                     </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="w-6 h-6 inline ml-1"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M6.32 4.68a1.75 1.75 0 00-2.49 0 1.75 1.75 0 000 2.48L10.94 14.5l-7.11 7.32a1.75 1.75 0 002.49 2.49l7.11-7.32 7.32 7.11a1.75 1.75 0 102.49-2.49l-7.32-7.11 7.32-7.11a1.75 1.75 0 10-2.49-2.49L12 10.94 6.32 4.68z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
                     </button>
                   </div>
                 </div>
