a133c582af7b136ae1a83c01c0eeb26642b7c2de
Zaim <Yousseftitazaim@gmail.com>
2024-06-07 18:21:59 +0100

    Update user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
   name: {type:String, unique:true, required:true},
   email: {type:String, unique:true, required:true},
   password: {type:String, unique:true, required:true},
-  profilePicture: {type:[String], default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png", required:false},
+  profilePicture: {type:[String], default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png", required:true},
 }, {timestamps:true});
 
 const UserModel = mongoose.model('User', UserSchema);
